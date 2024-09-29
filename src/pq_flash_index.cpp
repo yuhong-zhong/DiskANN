4e6c9f5c6921311e59a928b91a89f16a103f6002
Yuhong Zhong <yz@cs.columbia.edu>
2024-09-29 15:17:56 -0600

    Remove 10% caching constraint

--- src/pq_flash_index.cpp
+++ src/pq_flash_index.cpp
@@ -356,13 +356,13 @@ void PQFlashIndex<T, LabelT>::cache_bfs_levels(uint64_t num_nodes_to_cache, std:
     tsl::robin_set<uint32_t> node_set;
 
     // Do not cache more than 10% of the nodes in the index
-    uint64_t tenp_nodes = (uint64_t)(std::round(this->_num_points * 0.1));
-    if (num_nodes_to_cache > tenp_nodes)
-    {
-        diskann::cout << "Reducing nodes to cache from: " << num_nodes_to_cache << " to: " << tenp_nodes
-                      << "(10 percent of total nodes:" << this->_num_points << ")" << std::endl;
-        num_nodes_to_cache = tenp_nodes == 0 ? 1 : tenp_nodes;
-    }
+    // uint64_t tenp_nodes = (uint64_t)(std::round(this->_num_points * 0.1));
+    // if (num_nodes_to_cache > tenp_nodes)
+    // {
+    //     diskann::cout << "Reducing nodes to cache from: " << num_nodes_to_cache << " to: " << tenp_nodes
+    //                   << "(10 percent of total nodes:" << this->_num_points << ")" << std::endl;
+    //     num_nodes_to_cache = tenp_nodes == 0 ? 1 : tenp_nodes;
+    // }
     diskann::cout << "Caching " << num_nodes_to_cache << "..." << std::endl;
 
     // borrow thread data
